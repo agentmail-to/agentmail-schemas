90dfaa55c3ce1492964d1e378523301a83ad6cab
Haakam Aujla <haakamsingh21@gmail.com>
2026-01-30 09:27:53 -0800

    Update attachment schema

--- src/attachment.ts
+++ src/attachment.ts
@@ -2,10 +2,11 @@ import { z } from 'zod'
 
 export const AttachmentSchema = z.object({
     attachment_id: z.uuid(),
-    filename: z.string(),
-    content_type: z.string(),
+    filename: z.string().optional(),
     size: z.int(),
-    inline: z.boolean(),
+    content_type: z.string().optional(),
+    content_disposition: z.enum(['inline', 'attachment']),
+    content_id: z.string().optional(),
 })
 
 export type Attachment = z.infer<typeof AttachmentSchema>
